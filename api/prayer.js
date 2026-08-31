68d171bc8ce539ad9a5b086719b5f970525418cd
hamzamozy192-tech <hamzamozy192@gmail.com>
2026-09-01 00:29:09 +0300

    Implement prayer timings API handler
    Add API handler for prayer timings by city

--- api/prayer.js
+++ api/prayer.js
@@ -1,1 +1,12 @@
-
+export default async function handler(req, res) {
+  var city = req.query.city || "القاهرة";
+  try {
+    var url = "https://api.aladhan.com/v1/timingsByCity?city=" + encodeURIComponent(city) + "&country=Egypt&method=5";
+    var r = await fetch(url);
+    var data = await r.json();
+    res.setHeader("Cache-Control", "s-maxage=3600");
+    res.status(200).json(data);
+  } catch (e) {
+    res.status(500).json({ code: 500 });
+  }
+}
